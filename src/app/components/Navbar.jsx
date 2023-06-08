035da76f280aa450e397611f261150e334b0cedc
Daniele Maggiore <dmaggiore@sorint.it>
2023-06-08 10:31:30 +0200

    Added translations in Navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import LanguageSelector from "./LanguageSelector";
+import FormattedMessage from "./FormattedMessage";
 
 const Navbar = () => {
   return <div className="navbar">
     <div className="navbar__content">
       <div className="navbar__logo">Genti</div>
       <div className="navbar__list">
-        <div className="navbar__item">Home</div>
-        <div className="navbar__item">Products</div>
-        <div className="navbar__item">Products</div>
-        <div className="navbar__item">Clients</div>
+        <div className="navbar__item"><FormattedMessage id='navbar.home' /></div>
+        <div className="navbar__item"><FormattedMessage id='navbar.features' /></div>
+        <div className="navbar__item"><FormattedMessage id='navbar.products' /></div>
+        <div className="navbar__item"><FormattedMessage id='navbar.clients' /></div>
         <div className="navbar__lang"><LanguageSelector /></div>
       </div>
     </div>

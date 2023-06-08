4f6511b50fbcee806ff5de8db460a7e0426e10ff
Genti <genti.musaj@tesiengineering.com>
2023-06-08 10:20:42 +0200

    piccole modifiche

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
+import robot from "../../assets/png/robot.png";
 
 const Home = () => {
   return <div className="home">
     <div className="home__content">
       <div className="home__left">
-        <h1 className="home_title_firstline">
-          The Next <br className="home_text_br1" />{" "}
-          <span className="home_text_gradient">
+        <h1 className="home__title">
+          The Next <br className="home__title-br" />{" "}
+          <span className="home__title-highlight">
             Generation
           </span>{" "}
         </h1>
+        <h1 className="home__title-thirdline">
+          Payment Method.
+        </h1>
+        <p className="home__text">
+          Our team of experts uses a methodology <br className="home_text_br" />{" "}
+          to identify the credit cards most likely to fit your needs.
+        </p>
       </div>
 
-      <h1 className="home_text_secondline">
-          Payment Method.
-      </h1>
+      <div className="home__right">
+        <img src={robot} alt="billing" className="home__main-image" />
 
-      <p className="home_text">
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs.
-      </p>
+        {/* gradient start */}
+        <div className="" />
+        <div className="" />
+        <div className="" />
+        {/* gradient end */}
+      </div>
     </div>
   </div>
 }
